88444f548145baaf6b38329e3f7a80bd3fd7d4bc
Tim Curzon <timcurzon@users.noreply.github.com>
2017-07-17 13:44:07 +0100

    Minor logging / status ouput amendments in OpenDSA build script

--- x86-64/opendsa/build.sh
+++ x86-64/opendsa/build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-echo "Cleaning out previous build..."
+echo "Cleaning out previous export..."
 rm -rf export
 mkdir export
 
-echo "Rebuilding..."
+echo "Building..."
 docker build -t opendsa_build:latest .
-docker run --rm -p 8081:80 -v $PWD/export:/tmp/export opendsa_build 
+docker run --rm -d -p 8081:80 -v $PWD/export:/tmp/export opendsa_build
+
+printf "\nNOTICE! Build container is running in background, and will begin to serve build at http://localhost:8081 shortly...\n\n"
--- x86-64/opendsa/entrypoint.sh
+++ x86-64/opendsa/entrypoint.sh
@@ -3,6 +3,9 @@
 # Export build
 cp -R /tmp/build/OpenDSA /tmp/export/OpenDSA
 
+# Tidy up export
+rm -rf /tmp/export/OpenDSA/.git
+
 # Start basic webserver (demo build)
 cd /tmp/build/OpenDSA
 python server.py 0.0.0.0:80
